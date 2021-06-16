3c39ff85dfab7e377743af34d4fa393c074a69a4
Sbravenclaw <sbravenclaw@gmail.com>
2021-06-16 23:53:55 +0300

    bibaboba228

---
+++ de3/de3/Navifation.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace de3
+{
+    /// <summary>
+    /// Логика взаимодействия для Navifation.xaml
+    /// </summary>
+    public partial class Navifation : Page
+    {
+        public Navifation()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBiba_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new biba());
+        }
+
+        private void BtnBoba_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new boba());
+        }
+    }
+}
---
+++ de3/de3/boba.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace de3
+{
+    /// <summary>
+    /// Логика взаимодействия для boba.xaml
+    /// </summary>
+    public partial class boba : Page
+    {
+        public boba()
+        {
+            InitializeComponent();
+        }
+    }
+}
